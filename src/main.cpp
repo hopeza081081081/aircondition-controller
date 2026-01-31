05299daf906bb83985cafe5b926f7d0639d552ff
Tanakorn Khunkhao <tanakorn100@hotmail.com>
2026-01-31 17:34:16 +0700

    add more delay in void loop()

--- src/main.cpp
+++ src/main.cpp
@@ -242,7 +242,7 @@ void setup()
 void loop()
 {
     // Main loop is empty - all work is done in FreeRTOS tasks
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
 }
 
 // ========== OTA Task ==========
